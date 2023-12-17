919e7da1fc43a0b1efd613a27957394e0e4616fb
Thọ <54027634+tho2503@users.noreply.github.com>
2023-12-17 23:18:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = value; // Paste your API key here
+const API_KEY = process.env.API_KEY; // Paste your API key here
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
